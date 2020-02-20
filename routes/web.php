44d38a71b35a4fd5a7c937017805b24733abe185
Vijay Felix Raj C <vijayfelixraj@gmail.com>
2020-02-20 15:19:08 +0530

    Added Source for woocommerce RestAPI

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+
+Route::get('ical', 'IcalController@index')->name('ical');
+Route::get('ical/sync', 'IcalController@autoSync')->name('autoSync');
+
+Route::get('woo-commerce', 'WooCommerceController@index');
+// Route::get('woo-commerce', 'WooCommerceController@index');
